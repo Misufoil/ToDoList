81f36d6176b673edd29d14ca4a33d1ba0b856624
Maxim Teplov <teplov.maksimka03@mail.ru>
2023-05-11 00:34:35 +0300

    dialog version

--- app/src/main/java/com/example/to_do_list/MainActivity.kt
+++ app/src/main/java/com/example/to_do_list/MainActivity.kt
@@ -4,16 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.graphics.toColor
+import androidx.lifecycle.ViewModelProvider
 import com.example.to_do_list.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this)[TaskViewModel::class.java]
+
+        binding.newTaskButton.setOnClickListener {
+            NewTaskSheet().show(supportFragmentManager, "newTaskTag")
+        }
+
+        taskViewModel.desc.observe(this) {
+            binding.taskDesc.text = String.format("Task Desc: %s", it)
+        }
 
         var iconState = true
 
--- app/src/main/java/com/example/to_do_list/NewTaskSheet.kt
+++ app/src/main/java/com/example/to_do_list/NewTaskSheet.kt
@@ -1,35 +1,63 @@
 package com.example.to_do_list
 
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.*
 import android.widget.ArrayAdapter
+import android.widget.RelativeLayout
 import android.widget.Spinner
-import androidx.appcompat.widget.AppCompatButton
-import androidx.fragment.app.Fragment
-import com.example.to_do_list.databinding.ActivityMainBinding
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.to_do_list.databinding.FragmentNewTaskSheetBinding
 
 
-class NewTaskSheet : Fragment() {
+class NewTaskSheet : DialogFragment() {
 
     private lateinit var binding: FragmentNewTaskSheetBinding
+    private lateinit var taskViewModel: TaskViewModel
 
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = FragmentNewTaskSheetBinding.inflate(layoutInflater)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val activity = requireActivity()
+//        binding = FragmentNewTaskSheetBinding.inflate(layoutInflater)
+        taskViewModel = ViewModelProvider(activity).get(TaskViewModel::class.java)
 
         val spinner: Spinner = binding.spinImportance
+        val myStringArray = resources.getStringArray(R.array.priorities)
 
-        this.context?.let {
-            ArrayAdapter.createFromResource(it,
-                R.array.priorities,
-                android.R.layout.simple_spinner_item
-            ).also { adapter ->
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                spinner.setSelection(0)
-                spinner.adapter = adapter
+        val adapter = CustomArrayAdapter(this.requireContext(), android.R.layout.simple_spinner_item, myStringArray, 2)
+
+//        val customSpinnerLayout = android.R.layout.simple_spinner_item
+//        val adapter = object : ArrayAdapter<String>(this.requireContext(), customSpinnerLayout, myStringArray) {
+//
+//        }
+
+// устанавливаем созданный макет в Spinner
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.setSelection(0)
+        spinner.adapter = adapter
+
+
+//        this.context?.let {
+//            ArrayAdapter.createFromResource(it,
+//                R.array.priorities,
+//                android.R.layout.simple_spinner_item
+//            ).also { adapter ->
+//                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//                spinner.setSelection(0)
+//                spinner.adapter = adapter
+//            }
+//        }
+
+        binding.topAppBarNewTask.setOnMenuItemClickListener {menuItem ->
+            when(menuItem.itemId) {
+                R.id.action_save -> saveAction()
+                else -> false
             }
         }
     }
@@ -41,6 +69,65 @@ class NewTaskSheet : Fragment() {
     }
 
 
+    private fun saveAction(): Boolean {
+        taskViewModel.desc.value = binding.editDesc.text.toString()
+        binding.editDesc.setText("")
+        dismiss()
 
+        return true
+    }
+
+    class CustomArrayAdapter(context: Context, resource: Int, objects: Array<String>, private val selectedPosition: Int) : ArrayAdapter<String>(context, resource, objects) {
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val view = super.getView(position, convertView, parent)
+            val textView = view.findViewById<TextView>(android.R.id.text1)
+            if (position == selectedPosition) {
+                textView.setTextColor(ContextCompat.getColor(this.context, R.color.red))
+            } else {
+                textView.setTextColor(ContextCompat.getColor(this.context, R.color.black))
+            }
+            return view
+        }
+
+        override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val view = super.getDropDownView(position, convertView, parent)
+                val textView = view.findViewById<TextView>(android.R.id.text1)
+
+                // устанавливаем цвет текста для определенной строки
+                if (position == selectedPosition) {
+                    textView.setTextColor(ContextCompat.getColor(this.context, R.color.red))
+                } else {
+                    textView.setTextColor(ContextCompat.getColor(this.context, R.color.black))
+                }
+                return view
+        }
+    }
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        val view: View = inflater.inflate(R.layout.fragment_dialog, container, false)
+//
+//        // Set the dialog to full screen
+//        val window = dialog!!.window
+//        window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+//        return view
+//    }
+//
+//    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+//        val dialog = super.onCreateDialog(savedInstanceState)
+//        dialog.window?.apply {
+//            setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+//            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+//        }
+//        return dialog
+//    }
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setStyle(STYLE_NORMAL, R.style.FullScreenDialogTheme)
+//    }
 
 }
